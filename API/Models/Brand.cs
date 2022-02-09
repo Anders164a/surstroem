f424333e94398211f3b39c06374b2f3b3f059f6e
DavidSKoppel <davi3442@edu.sde.dk>
2022-02-09 12:15:19 +0100

    Updated to current database

---
+++ API/Models/Brand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace surstroem.Models
+{
+    public partial class Brand
+    {
+        public Brand()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ API/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace surstroem.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            EmployeeHasShifts = new HashSet<EmployeeHasShift>();
+        }
+
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? WarehouseId { get; set; }
+        public int? WorkPhone { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual ICollection<EmployeeHasShift> EmployeeHasShifts { get; set; }
+    }
+}
